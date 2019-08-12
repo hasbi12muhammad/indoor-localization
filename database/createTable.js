eebafee7172069f00fdb9aac41db17942ae8581a
hasbi12muhammad <hasbi12.muhammad@gmail.com>
2019-08-12 15:34:02 +0700

    first upload

---
+++ database/createTable.js
@@ -0,0 +1,20 @@
+const db = require('./config_db');
+
+db.serialize(function(){
+
+    let sql = `CREATE TABLE IF NOT EXISTS rssi_table(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ruang VARCHAR(64),
+        date_time DATETIME,
+        beacon1 INT(64),
+        beacon2 INT(64)
+
+    );`;
+    db.run(sql, (err) => {
+        if(err) throw err;
+        console.log("Table created");
+    });
+
+});
+
+db.close();
---
+++ index.js
@@ -0,0 +1,59 @@
+const http = require('http');
+let db = require('D:/DATA KULIAH/SEMESTER VIII/SKRIPSI!!!/SERVER/database/config_db.js');
+
+const mac1 = "3C:71:BF:C4:E1:F4";
+const mac2 = "B4:E6:2D:B7:72:45";
+
+let post = []
+let rssi = []
+let rssi1
+let rssi2
+//create a server object:
+http.createServer(function (req, res) {
+    //end the response
+    let body = [];
+    let url = req.url
+    if (url == "/tahap1") {
+        let tes = req.on('data', (chunk) => {
+            body.push(chunk)
+            //console.log(body)
+        }).on('end', () => {
+            post = JSON.parse(body.toString())
+
+            if (post.mac == mac1) {
+                rssi[0] = post.rssi
+            } else if (post.mac == mac2) {
+                rssi[1] = post.rssi
+            }
+            if (rssi[0] == null || rssi[1] == null) {
+                console.log("waiting another rssi...")
+            } else if (rssi[0] != null && rssi[1] != null) {
+                db.serialize(function () {
+                    let sql = `INSERT INTO rssi_table (ruang,date_time,beacon1,beacon2) VALUES (?,?,?,?)`;
+                    let stmt = db.prepare(sql);
+                    var values = [
+                        ["Ruang 1", new Date(Date.now()).toString(), rssi[0], rssi[1]]
+                    ];
+                    console.log(post)
+                    values.forEach((value) => {
+                        stmt.run(value, (err) => {
+                            if (err) throw err;
+                        });
+                    });
+                    console.log(`${values.length} record inserted`);
+                    stmt.finalize();
+                });
+                rssi[0] = null
+                rssi[1] = null
+            }
+        })
+        res.write("tahap1"); //write a response to the client
+        res.end();
+    } else if (url == "/tahap2") {
+        res.write("tahap2"); //write a response to the client
+        res.end();
+    } else {
+        res.write("not found"); //write a response to the client
+        res.end();
+    }
+}).listen(8080); //the server object listens on port 8080 
